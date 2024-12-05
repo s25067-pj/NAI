e60f2bf8ddaccb25ab63e11ebdd2accf0bedad2a
s24586 <s24586@pjwstk.edu.pl>
2024-12-05 18:55:26 +0100

    last small changes

--- NAI4/SVM.py
+++ NAI4/SVM.py
@@ -3,18 +3,26 @@ import matplotlib.pyplot as plt
 from sklearn import svm
 from sklearn.model_selection import train_test_split
 
+""""Wczytywanie pliku,skipnięcie pierwszego weirsza"""
 input_file = './Decision_tree/data_decision_trees.txt'
 data = np.loadtxt(input_file, delimiter=';', skiprows=1)
-X, y = data[:, :-1], data[:, -1]
 
+"""" Podział danych na cechy (X) i etykiety (y) i Konwersja ocen wina ponizej 6 (dobre wino: 1, złe wino: 0)"""
+X, y = data[:, :-1], data[:, -1]
 y = np.where(y >= 6, 1, 0)
 
+
+""""Wybór tylko dwóch pierwszych cech do wizualizacji,podział na zbiór treningowy i testowy i tworzenie modelu svm:
+- svc - uczenie modelu treningowymi danymi
+- gamma- Jak bardzo model patrzy/uważa na szczegóły
+- kernel-rbf dzieli na rozne linie w przeciwienstwie do kernel='linear' przydaje sie bardziej jak wartosci sa wymieszane
+- C1 - kontroluje, jak bardzo model unika błędów w danych.
+"""
 X = X[:, :2]
-
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.25, random_state=5)
-
 svc = svm.SVC(kernel='rbf', C=1, gamma=100).fit(X_train, y_train)
 
+""" Tworzenie siatki i przechodzenie przez kazdy punkt z ocenami"""
 x_min, x_max = X[:, 0].min() - 1, X[:, 0].max() + 1
 y_min, y_max = X[:, 1].min() - 1, X[:, 1].max() + 1
 h = (x_max - x_min) / 100
@@ -24,9 +32,9 @@ xx, yy = np.meshgrid(np.arange(x_min, x_max, h),
 Z = svc.predict(np.c_[xx.ravel(), yy.ravel()])
 Z = Z.reshape(xx.shape)
 
+""""Tworzenie wykresów """
 plt.figure()
 plt.contourf(xx, yy, Z, cmap=plt.cm.Paired, alpha=0.8)
-
 plt.scatter(X[:, 0], X[:, 1], c=y, cmap=plt.cm.Paired, edgecolors='k', s=75)
 plt.xlabel('Feature 1')
 plt.ylabel('Feature 2')
--- NAI4/readme.md
+++ NAI4/readme.md
@@ -1,15 +1,15 @@
 Cel projektu:
+Celem projektu jest zbudowanie modelu klasyfikacji, który rozróżnia między dobrym a złym winem na podstawie wybranych 
+cech chemicznych. Projekt wykorzystuje algorytm drzewa decyzyjnego do klasyfikacji i wizualizacji wyników.
 
 Autorzy projektu:
 Paulina Debis s25067
 Krystian Jank s24586
 
-Aby uruchomić projekt należy wpisać w terminalu:
-
-
-
 Jeśli posiadamy zainstalowanego pythona wystarczy zainstalować biblioteki użyte w projekcie za pomocą:
 pip install numpy
 pip install scikit-learn
+pip install matplotlib
+
 
 
